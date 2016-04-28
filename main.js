1066915691a89577e65afe71126e401942e39401
august-of-wind <ameyer5@luc.edu>
2016-04-28 15:05:47 -0500

    First pages commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function showMyDiv(){
+  document.getElementById('page0').style.display = "none";
+  document.getElementById('page1').style.display = "";
+}
+
+function showOtherDiv(){
+  document.getElementById('page1').style.display = "none";
+  document.getElementById('page0').style.display = "";
+}
+
